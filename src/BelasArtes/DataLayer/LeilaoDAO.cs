672bf1c83dee72a83a926c143b88086a16157e00
HenriqueMalheiro <91957935+HenriqueMalheiro@users.noreply.github.com>
2024-01-18 11:28:24 +0000

    DAOs

--- src/BelasArtes/DataLayer/LeilaoDAO.cs
+++ src/BelasArtes/DataLayer/LeilaoDAO.cs
@@ -11,4 +11,44 @@ public class LeilaoDAO : ILeilaoDAO
         string sql = "select * from Leilao";
         return _db.LoadData<Leilao, dynamic>(sql, new { });
     }
+    
+    public async Task<Leilao?> GetById(int leilaoId)
+    {
+        string sql = "select * from Leilao where Id = @LeilaoId";
+        var parameters = new { LeilaoId = leilaoId };
+
+        List<Leilao> leilaoList = await _db.LoadData<Leilao, dynamic>(sql, parameters);
+        return leilaoList.FirstOrDefault()!;
+    }
+
+    public async Task<bool> PutLeilao(Leilao leilao){
+        string mergeSql = @"
+            MERGE INTO Leilao AS target
+            USING (VALUES (DEFAULT)) AS source (id)
+            ON target.id = source.id
+            WHEN MATCHED THEN
+                UPDATE SET 
+                    target.data_fim = @data_fim,
+                    target.preco_inicial = @preco_inicial,
+                    target.comprador_email = @comprador_email,
+                    target.pintura_id = @pintura_id,
+                    target.id_estado = @id_estado
+            WHEN NOT MATCHED THEN
+                INSERT (data_inicio, data_fim, preco_inicial, comprador_email, pintura_id, id_estado)
+                VALUES (@data_inicio, @data_fim, @preco_inicial, @comprador_email, @pintura_id, @id_estado);";
+
+        var parameters = new{
+            leilao.DataInicio,
+            leilao.DataFim,
+            leilao.PrecoInicial,
+            leilao.EmailComprador,
+            leilao.CodPintura,
+            leilao.CodEstado
+        };
+
+        await _db.SaveData(mergeSql, parameters);
+
+        // If rowsAffected is greater than 0, the operation was successful
+        return true;
+    }
 }
