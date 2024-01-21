491ed98ad6c6c513a97b16918218d5ef1817e65a
HenriqueMalheiro <91957935+HenriqueMalheiro@users.noreply.github.com>
2024-01-21 20:45:54 +0000

    leiloes

--- src/BelasArtes/DataLayer/Pintura.cs
+++ src/BelasArtes/DataLayer/Pintura.cs
@@ -9,11 +9,7 @@ public class Pintura {
     public double? Largura { get; set; }
     public double? Peso { get; set; }
     public string Descricao { get; set; } = "";
-<<<<<<< HEAD
     public byte[]? Foto { get; set; }
-=======
-    public Byte[]? Foto { get; set; }
->>>>>>> d94d33ad15fe0f0771df3d4d70c91e4d9de8f9c7
     public string Artista { get; set; } = "";
     public int? AnoCriacao { get; set; }
     public bool? Original { get; set; }
@@ -21,15 +17,8 @@ public class Pintura {
     public string? EmailVendedor { get; set; } 
     public int? CodMovimentoArtistico { get; set; }
 
-<<<<<<< HEAD
     public Pintura(int? id, string nome, double? altura, double? largura, double? peso, string descricao,
     byte[]? foto, string artista, int? anoCriacao, bool? original,
-=======
-    public Pintura(){}
-
-    public Pintura(int? id, string nome, float? altura, float? largura, float? peso, string descricao,
-    Byte[]? foto, string artista, int? anoCriacao, bool? original,
->>>>>>> d94d33ad15fe0f0771df3d4d70c91e4d9de8f9c7
     bool? verificacaoAutenticidade, string? emailVendedor, int? codMovimentoArtistico){
         Id = id;
         Nome = nome;
