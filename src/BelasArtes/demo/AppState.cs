3e2ae2f7d954825fb16f9a1c73ed1b70d21f299b
HenriqueMalheiro <91957935+HenriqueMalheiro@users.noreply.github.com>
2024-01-22 18:24:43 +0000

    leiloes

--- src/BelasArtes/demo/AppState.cs
+++ src/BelasArtes/demo/AppState.cs
@@ -1,5 +1,7 @@
 using DataLayer;
 
 public class AppState{
+    //Atributos para a página com os diversos leilões
     public List<Leilao> ListLeiloesMostrar{ get; set;} = new List<Leilao>();
+    public bool buttonOrdenarSelecionado{get;set;} = false;
 }
