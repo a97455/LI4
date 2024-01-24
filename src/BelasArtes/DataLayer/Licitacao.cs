8d7c28bad8326677d307f04162a59debee1025ea
HenriqueMalheiro <91957935+HenriqueMalheiro@users.noreply.github.com>
2024-01-24 18:13:19 +0000

    auction

--- src/BelasArtes/DataLayer/Licitacao.cs
+++ src/BelasArtes/DataLayer/Licitacao.cs
@@ -5,13 +5,13 @@ namespace DataLayer;
 public class Licitacao {
         public int? Id { get; set; }
         public double? Valor { get; set; }
-        public string? EmailLicitador { get; set; }
+        public string EmailLicitador { get; set; } = "";
         public DateTime? Data { get; set; }
         public int? IdLeilao {get;set;}
 
         public Licitacao(){}
 
-        public Licitacao(int? id, double? valor, string? emailLicitador, DateTime? data, int? idLeilao){
+        public Licitacao(int? id, double? valor, string emailLicitador, DateTime? data, int? idLeilao){
             Id = id;
             Valor = valor;
             EmailLicitador = emailLicitador;
